ab99f3179aa2b19b4d18ffce006303a2b40e4063
shubhang-arora <sa287@snu.edu.in>
2016-11-23 02:17:45 +0530

    Returning Tags for uploaded photos

--- app/Api/V1/Controllers/UploadController.php
+++ app/Api/V1/Controllers/UploadController.php
@@ -13,12 +13,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Password;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Dingo\Api\Exception\ValidationHttpException;
+use Carbon\Carbon;
 
 class UploadController extends Controller
 {
     use Helpers;
 
-    public function upload(Request $request)
+    public function uploads(Request $request)
     {
         $validator = Validator::make($request->file(),[
             'file'   =>  'required|image'
@@ -29,6 +30,7 @@ class UploadController extends Controller
         }
         $types = array('jpeg', 'png' ,'jpg');
         $file = $request->file('file');
+
         // $size = $file->getClientSize();
         $fileFormat = $file->getClientOriginalExtension();
 
@@ -38,19 +40,46 @@ class UploadController extends Controller
             $filename = $time . '.' . $fileFormat;
             $destinationPath = 'uploads';
             $link = $destinationPath . '/' . $filename;
-            $status = move_uploaded_file($file,'/var/www/html/asque/public/'.$link);
+            $status = move_uploaded_file($file,$link);
 
-            if($status)
-            {
-                $move = shell_exec("sudo mv ../../../public/".$link." /var/www/html/asque/public/".$link);
+            $curl = curl_init();
 
-                return $this->response()->error($link,200);
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://api.clarifai.com/v1/token/",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS =>  array(
+                     "client_id" => '3TdR8-hfkTjOF5oXCx9eA2oPK1ZHNZcY7OjcQdVA',
+                     "client_secret" => 'FIsp8oqtp9S-JvfLMt5lkfvciJFiRkxNUIQEpztt',
+                    "grant_type"    =>  'client_credentials'
+                 )
+            ));
 
-            }
-            return $this->response()->errorBadRequest("There was a problem in Uploading");
-        } else {
+            $json_response = json_decode(curl_exec($curl), true);
+            $access_token = $json_response["access_token"];
+            curl_close($curl);
 
-            return $this->response()->error("Wrong file format",422);
+            $image = curl_init();
+
+            curl_setopt_array($image, array(
+                CURLOPT_URL => "https://api.clarifai.com/v1/tag/?model=general-v1.3&url=http://weknowyourdreams.com/images/sad/sad-05.jpg",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => array('Authorization: Bearer '.$access_token)
+            ));
+
+            $response = json_decode(curl_exec($image), true);
+
+            $result = ($response["results"][0]["result"]["tag"]["classes"]);
+            return json_encode($result);
         }
     }
 }
